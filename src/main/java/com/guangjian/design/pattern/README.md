d65c00f2bab892e9adf974f173590485ba10e4a0
杨光剑 <yangguangjian@rhytech.com.cn>
2023-12-14 09:13:02 +0800

    新功能: 责任链模式

---
+++ src/main/java/com/guangjian/design/pattern/README.md
@@ -0,0 +1,29 @@
+# 
+
+* behavioral 行为型模式
+  * chainofresponsibility 责任链模式
+  * command 命令模式
+  * interpreter 解释器模式
+  * iterator 迭代器模式
+  * mediator 终结者模式
+  * memento 备忘录模式
+  * observer 观察者模式
+  * state 状态模式
+  * strategy 策略模式
+  * templatemethod 模板方法模式
+  * visitor 访问者模式
+* creational 创建型模式
+  * abstractfactory 抽象工厂
+  * builder 建造者模式
+  * factorymethod 工厂方法模式
+  * prototype 原型模式
+  * simplefactory 简单工厂模式
+  * singleton 单例模式
+* structural 结构型模式
+  * adapter 适配器模式
+  * bridge 桥接模式
+  * composite 组合模式
+  * decorator 装饰器模式
+  * facade 外观模式
+  * flyweight 享元模式
+  * proxy 代理模式
---
+++ src/main/java/com/guangjian/design/pattern/behavioral/chainofresponsibility/runoob/Test.java
@@ -0,0 +1,31 @@
+package com.guangjian.design.pattern.behavioral.chainofresponsibility.runoob;
+
+/**
+ * @author yanggj
+ * @version 1.0.0
+ * @date 2023/12/14 9:03
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        AbstractLogger loggerChain = getChainOfLoggers();
+
+        loggerChain.logMessage(AbstractLogger.INFO, "This is an information.");
+
+        loggerChain.logMessage(AbstractLogger.DEBUG, "This is a debug level information.");
+
+        loggerChain.logMessage(AbstractLogger.ERROR, "This is an error information.");
+    }
+
+    private static AbstractLogger getChainOfLoggers() {
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
+    }
+}
