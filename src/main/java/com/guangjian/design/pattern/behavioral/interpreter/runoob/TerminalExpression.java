d4152689344946ef7ff39826d1d6b6c2d5fad9e4
guangjian-yang <guangjian.yang@qq.com>
2023-12-18 18:26:52 +0800

    新功能: 解释器模式

---
+++ src/main/java/com/guangjian/design/pattern/behavioral/interpreter/runoob/TerminalExpression.java
@@ -0,0 +1,23 @@
+package com.guangjian.design.pattern.behavioral.interpreter.runoob;
+
+/**
+ * @author yanggj
+ * @version 1.0.0
+ * @date 2023/12/18 18:24
+ */
+public class TerminalExpression implements Expression {
+
+    private String data;
+
+    public TerminalExpression(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        if (context.contains(data)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/guangjian/design/pattern/behavioral/interpreter/runoob/Test.java
@@ -0,0 +1,9 @@
+package com.guangjian.design.pattern.behavioral.interpreter.runoob;
+
+/**
+ * @author yanggj
+ * @version 1.0.0
+ * @date 2023/12/18 18:25
+ */
+public class Test {
+}
